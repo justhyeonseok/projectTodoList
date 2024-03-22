8b906bf35aecb2290fc27c3d91af37de04cddc17
hyeonseok <ghostpar12@naver.com>
2024-03-22 21:02:30 +0900

    [Fix] 잠시 주석처리

--- src/test/kotlin/com/todoproject/todolist/TodoListListApplicationTests.kt
+++ src/test/kotlin/com/todoproject/todolist/TodoListListApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.todoproject.todolist
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class TodoListListApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//package com.todoproject.todolist
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class TodoListListApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
